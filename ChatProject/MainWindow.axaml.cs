df18bb0f50ba94df80634608aba055131ab5bcba
AntonP1170 <parsukanton80@gmail.com>
2026-09-04 18:06:45 +0300

    first commit

---
+++ ChatProject/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Avalonia.Controls;
+
+namespace ChatProject;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Console.WriteLine("Hello from Anton!");
+    }
+}
