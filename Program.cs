da7e74e3924410b5cadb25094631eb05d644c26d
AntonP1170 <parsukanton80@gmail.com>
2026-09-08 18:16:21 +0300

    fix some bugs and add grid template

--- Program.cs
+++ Program.cs
@@ -9,9 +9,8 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args)
-        .StartWithClassicDesktopLifetime(args);
-
+    public static void Main(string[] args)=> BuildAvaloniaApp()
+    .StartWithClassicDesktopLifetime(args);
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
--- obj/Debug/net10.0/ChatProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/ChatProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba508682aa4745a82e2320056e7381ddfedb33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
